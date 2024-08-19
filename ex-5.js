a22148765330a222805df629957d2bd293d71e6f
jerpuen <kittipot.kulpinit@gmail.com>
2024-08-19 11:03:20 +0700

    sent: assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalPrice = 27000;
+let discountPercent = 25;
+let netPrice = (totalPrice * (100 - discountPercent)) / 100;
+console.log(netPrice); //ราคาสุทธิ
